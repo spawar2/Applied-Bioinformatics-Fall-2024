3e211e231eb064b63565af79b5a5f06a3c028804
Pawar <pawar1550@gmail.com>
2024-09-19 10:41:40 -0400

    Add files via upload

---
+++ Phylogenetic-Analysis.R
@@ -0,0 +1,7 @@
+#Author: Pawar; Date: September 18, 2024; Purpose: Create dummy data, generate phylogenetic tree plot.
+
+# Load tree libraries
+library(ape)
+
+# Generate some dummy data
+text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark)"
