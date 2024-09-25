ea173e67f1a12b780e3c7c896e11a0c85b41221b
Pawar <pawar1550@gmail.com>
2024-09-25 12:24:54 -0400

    Add files via upload

--- Phylogenetic-Analysis.R
+++ Phylogenetic-Analysis.R
@@ -11,3 +11,10 @@ vert.tree<-read.tree(text=text.string)
 
 # Generate a basic plot of dummy data
 plot(vert.tree,no.margin=TRUE,edge.width=2, main="Phylogenetic tree")
+
+# Generate a un-rooted plot on the dummy data
+plot(unroot(vert.tree),type="unrooted",no.margin=TRUE,lab4ut="axial", edge.width=2)
+
+
+
+
